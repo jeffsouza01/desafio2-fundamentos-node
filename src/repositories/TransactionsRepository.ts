5be1cd71c18a5daf6b8eff5ff5696c2e9f1b914a
Jefferson da Silva <je_jss@hotmail.com>
2020-08-14 02:55:45 -0300

    :white_check_mark: Challenge Finish

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,40 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acumulator, transaction) => {
+        if (transaction.type === 'income') {
+          acumulator.income += transaction.value;
+        } else {
+          acumulator.outcome += transaction.value;
+        }
+        return acumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: TransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      type,
+      value,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
